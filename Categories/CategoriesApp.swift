47430590698163a1dce33a1a1e16ae0f9fa75da7
Stephen Hanna <stephenh126_@hotmail.co.uk>
2023-09-21 20:26:54 +0100

    Initial Commit

---
+++ Categories/CategoriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CategoriesApp.swift
+//  Categories
+//
+//  Created by Steve on 21/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct CategoriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
